b718837e128e0702b0e3fce79c3c0bfdbf845e04
Eddie Boyle <eddie_boyle@trimble.com>
2020-04-09 12:52:13 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AutofacStackOverflow {
+  using Autofac;
+
+  interface IAmATest<T> { }
+
+  class TestStackOverflow
+    : IAmATest<int>,
+      IAmATest<string>,
+      IAmATest<double> { }
+
+  class TestStackOverflowDecorator<T> : IAmATest<T> {
+    public TestStackOverflowDecorator(IAmATest<T> test) { }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      var builder = new ContainerBuilder();
+
+      builder.RegisterAssemblyTypes(typeof(Program).Assembly).AsClosedTypesOf(typeof(IAmATest<>));
+
+      builder.RegisterGenericDecorator(
+        typeof(TestStackOverflowDecorator<>),
+        typeof(IAmATest<>),
+        ctx => !ctx.ImplementationType.IsGenericType ||
+               ctx.ImplementationType.GetGenericTypeDefinition() != typeof(TestStackOverflowDecorator<>));
+
+      var container = builder.Build();
+      var test = container.Resolve<IAmATest<int>>();
+    }
+  }
+}
